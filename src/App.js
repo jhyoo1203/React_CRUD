9ca86d2545b205c3e9f1131577fa636e2a2d505c
YooJaeHyeon <jhyoo1203@gmail.com>
2022-05-29 16:23:13 +0900

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './style.css';
 
+function Header(){
+  return <header>
+    <h1><a href="/">WEB</a></h1>
+  </header>
+}
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+}
+function Article(){
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
 export default function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
